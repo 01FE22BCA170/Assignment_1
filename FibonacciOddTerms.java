4bf39b703b1c280d418c0f3ac6bddbb79cd209a0
Suraj-Umarani <01fe22bca170@kletech.ac.in>
2024-02-07 17:26:01 +0530

    Code for odd terms in Fibonacci

---
+++ FibonacciOddTerms.java
@@ -0,0 +1,19 @@
+public class FibonacciOddTerms {
+    
+    public static void main(String[] args) {
+        int n = 10; // Number of terms in the series
+        int a = 0, b = 1;
+        
+        System.out.println("Fibonacci series with only odd terms:");
+        
+        for (int i = 1; i <= n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+            
+            if (sum % 2 != 0) {
+                System.out.print(sum + " ");
+            }
+        }
+    }
+}
